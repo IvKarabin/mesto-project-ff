abff049d0688e3f58ccf60e975cb7617793b332a
Ivan Karabinyuk <ik645@yandex.ru>
2024-11-24 16:07:12 +0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -12,21 +12,25 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 function createCard(cardDesc, deleteCardCall) {
+  
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
 
   cardElement.querySelector('.card__title').textContent = cardDesc.name;
   cardElement.querySelector('.card__image').src = cardDesc.link;
-  cardElement.querySelector('.card__image').alt = `"Картинка ${cardDesc.name}"`;
+  cardElement.querySelector('.card__image').alt = `"Фотография ${cardDesc.name}"`;
   cardElement.querySelector('.card__delete-button').addEventListener('click', function() {deleteCardCall(cardElement);});
 
   return cardElement;
 };
 
 function deleteCard(cardElement) {
+  
   cardElement.remove();
 };
 
 initialCards.forEach(function (card) {
+  
   const cardElement = createCard(card, deleteCard);
+  
   placesList.append(cardElement);
 });
