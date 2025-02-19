20d3419eb9265639f9fdeb849db7a6812fef627d
123456 <ari@MacBook-Pro-123456.local>
2025-02-19 17:54:32 +0300

    for pull from new device

--- scripts/index.js
+++ scripts/index.js
@@ -7,34 +7,3 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
-
-const cardTemplate = document.querySelector('#card-template').content;
-const placesList = document.querySelector('.places__list');
-
-function createCard(cardDesc, deleteCardCall) {
-  
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  const cardImage = cardElement.querySelector('.card__image');
-  
-  cardElement.querySelector('.card__title').textContent = cardDesc.name;
-  cardImage.src = cardDesc.link;
-  cardImage.alt = `"Фотография ${cardDesc.name}"`;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
-    
-    deleteCardCall(cardElement);
-  });
-
-  return cardElement;
-};
-
-function deleteCard(cardElement) {
-  
-  cardElement.remove();
-};
-
-initialCards.forEach(function (card) { 
-
-  const cardElement = createCard(card, deleteCard); 
-
-  placesList.append(cardElement); 
-}); 
